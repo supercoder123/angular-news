8b6c213cc9017b588800b8c57f06fae105fa95f4
ashley.fernandes <ashley.fernandes@rksv.in>
2019-05-18 13:58:50 +0530

    changed card-list to card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import {News} from '../../news';
 
 @Component({
-  selector: 'app-card-list',
-  templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
-export class CardListComponent implements OnInit {
+export class CardComponent implements OnInit {
 
   @Input() post : News[];
 
